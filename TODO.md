67ecf89da74429f6a21ee7d2966fda75b1c242ef
alxndrpsclt <alxndr.psclt@gmail.com>
2019-11-30 15:36:05 +0100

    Adds showing touch input + structure of gesture in TODO

---
+++ TODO.md
@@ -0,0 +1,16 @@
+[
+  {
+    start_time: 225623,
+    start_pos: (83.256, 67.125),
+    start_pos_grid: (1, 5)  # Start position in the 3x5 grid on screen; just to know where the gesture is happening
+    gesture: "UUULL"     #U: up, D: down, L:left, R:right, A:down-left, B:down-right, I:up-left, J:up-right
+  }
+
+  {
+    start_time: 225623,
+    start_pos: (83.256, 67.125),
+    start_pos_grid: (1, 5)  # Start position in the 3x5 grid on screen; just to know where the gesture is happening
+    gesture: "UUULL"     #U: up, D: down, L:left, R:right, A:down-left, B:down-right, I:up-left, J:up-right
+  }
+
+]
--- testinput.py
+++ testinput.py
@@ -3,16 +3,20 @@ li = LibInput(context_type=ContextType.PATH)
 
 device = li.add_device('/dev/input/event1')
 
-for event in li.events:
-    if event.type.is_touch():
-        e={'type':event.type}
-        if event.type==EventType.TOUCH_DOWN:
-            e['coords']=event.coords
-            e['slot']=event.slot
-        if event.type==EventType.TOUCH_MOTION:
-            e['coords']=event.coords
-            e['slot']=event.slot
-        #if event.type==EventType.TOUCH_FRAME:
-        print(e)
 
+def main():
+    list_events=[]
+    for e in li.events:
+        if e.type.is_touch():
+            list_events.append(e)
+            if e.type==EventType.TOUCH_FRAME:
+                print([e.time, e.type.name])
+            if e.type in [EventType.TOUCH_DOWN, EventType.TOUCH_MOTION]:
+                print([e.time, e.type.name, e.coords, e.slot])
+                #print([e.time, e.type, e.device, e.coords, e.transform_coords, e.seat_slot, e.slot])
+
+# TODO: 
+
+if __name__ == '__main__':
+    main()
 
